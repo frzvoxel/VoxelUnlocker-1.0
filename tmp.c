91bd27d756a70558a404954862c66af50704f9ad
Voxel. <tux@xyecoc.com>
2026-03-19 17:57:16 +1000

    result2 and result 1 are displayed in one line

--- tmp.c
+++ tmp.c
@@ -7,8 +7,7 @@
 void tmp_page() {
 	Intro();
 
-	int result = system("del /q /s %temp%\\*");
-	int result2 = system("del /q /s C:\\Windows\\Temp\\*");
+	int result = system("del /q /s %temp%\\*"), result2 = system("del /q /s C:\\Windows\\Temp\\*");
 
 	if (result == 0 && result2 == 0) {
 		printf("Successfully cleaned temp :D ");
